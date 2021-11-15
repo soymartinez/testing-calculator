40e8f4e85c7f9d8ba3ebbd8a43126914b7588c0c
AlvaroMartinezMartinez <72507996+AlvaroMartinezMartinez@users.noreply.github.com>
2021-11-15 12:46:13 -0600

    landing-page v0.1 #1

--- src/app/landing-page/landing-page.component.spec.ts
+++ src/app/landing-page/landing-page.component.spec.ts
@@ -1,25 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { LandingPageComponent } from './landing-page.component';
 
 describe('LandingPageComponent', () => {
-  let component: LandingPageComponent;
-  let fixture: ComponentFixture<LandingPageComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LandingPageComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        LandingPageComponent
+      ],
+    }).compileComponents();
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LandingPageComponent);
-    component = fixture.componentInstance;
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(LandingPageComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should render the name', () => {
+    const fixture = TestBed.createComponent(LandingPageComponent);
     fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.box-text h2')?.textContent).toContain('Alvaro Martinez' || 'S19004874');
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render the license plate', () => {
+    const fixture = TestBed.createComponent(LandingPageComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.box-text h2')?.textContent).toContain('Alvaro Martinez' || 'S19004874');
   });
 });
