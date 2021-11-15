72f44eb8bafdeb071611a090aad7d3b505d9d9b5
AlvaroMartinezMartinez <72507996+AlvaroMartinezMartinez@users.noreply.github.com>
2021-11-15 12:29:36 -0600

    initial ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UiComponent } from './ui/ui.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UiComponent
   ],
   imports: [
     BrowserModule
