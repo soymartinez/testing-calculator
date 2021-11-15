6ac07a388e9b01642ebca2875182da44d3a415c2
AlvaroMartinezMartinez <72507996+AlvaroMartinezMartinez@users.noreply.github.com>
2021-11-15 13:03:43 -0600

    ui v0.1 #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UiComponent } from './ui/ui.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { UiComponent } from './ui/ui.component';
     UiComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ui/ui.component.ts
+++ src/app/ui/ui.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { circle } from '../circle/circle.component';
 
 @Component({
   selector: 'app-ui',
@@ -10,6 +11,19 @@ export class UiComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  result = '0.0';
+  radio = '';
+  
+  area() {
+    // this.result = circle(this.radio)
+
+    if (this.radio == '' || this.radio == null) {
+      this.result = 'Error';
+    }
+  
+    this.radio = ''
   }
 
 }
